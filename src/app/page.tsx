8f941bb1365e43030ffca26a7c8beb658771db03
imopbuilder <opbuilder9@gmail.com>
2024-01-02 17:56:46 +0530

    feat: add hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,21 @@
 import Header from '@/components/global/header';
+import Image from 'next/image';
 import { Fragment } from 'react';
 
 export default function Home() {
 	return (
 		<Fragment>
 			<Header />
-			<main className='h-[500vh]'>
-				<div>hello world</div>
+			<main className='pr-5'>
+				<section>
+					<div className='mx-[4%]'>
+						<div className='pt-24'>
+							<h2 className='text-8xl font-bold pb-12'>Op Blog.</h2>
+							<hr className='border-muted-foreground/60' />
+							<Image src={'/ui/hero-image.png'} className='w-full h-auto mt-4' width={100} height={100} alt='hero-image' loading='lazy' unoptimized />
+						</div>
+					</div>
+				</section>
 			</main>
 		</Fragment>
 	);
