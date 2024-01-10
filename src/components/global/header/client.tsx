36b9b80835bebbdcea31c60ce3ba69cb0e9e6fb2
imopbuilder <opbuilder9@gmail.com>
2024-01-10 15:29:58 +0530

    feat: add scrollbar to header component

--- src/components/global/header/client.tsx
+++ src/components/global/header/client.tsx
@@ -3,8 +3,8 @@
 import { motion, useScroll, useSpring } from 'framer-motion';
 
 export function Scrollbar() {
-	const { scrollYProgress } = useScroll();
-	const scaleX = useSpring(scrollYProgress);
+    const { scrollYProgress } = useScroll();
+    const scaleX = useSpring(scrollYProgress);
 
-	return <motion.div style={{ scaleX }} className='h-[1px] w-full bg-muted-foreground/40 z-10 origin-left absolute top-0 left-0' />;
+    return <motion.div style={{ scaleX }} className='h-[1px] w-full bg-muted-foreground/40 z-10 origin-left absolute top-0 left-0' />;
 }
--- src/components/global/header/index.tsx
+++ src/components/global/header/index.tsx
@@ -1,12 +1,13 @@
 import Link from 'next/link';
 import { Fragment } from 'react';
 import { ThemeToggle } from '../theme-toggle';
+import { Scrollbar } from './client';
 
 export default function Header() {
     return (
         <Fragment>
-            <header className='border-b px-[4%]'>
-                <div className='max-w-maxi mx-auto h-16 flex items-center justify-between'>
+            <header className='sticky top-0 backdrop-blur-md bg-background/15'>
+                <div className='max-w-maxi mx-auto px-[4%] h-16 flex items-center justify-between'>
                     <div>
                         <Link href={'/'} className='font-semibold'>
                             Op Blog
@@ -16,11 +17,10 @@ export default function Header() {
                         <ThemeToggle />
                     </nav>
                 </div>
+                <div className='w-full bg-muted-foreground/20 h-[1px] relative overflow-hidden'>
+                    <Scrollbar />
+                </div>
             </header>
-
-            {/* <div className='w-full bg-muted-foreground/20 h-[1px] relative'>
-                        <Scrollbar />
-                    </div> */}
 
             {/* <div className='fixed top-0 right-4 w-[1px] h-screen bg-muted-foreground/20' />
 			<div className='fixed top-[8.5px] right-[8.25px] size-4 bg-[#e3e3e3] dark:bg-[#292929] rotate-45 origin-center' /> */}
